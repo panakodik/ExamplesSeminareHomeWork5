098a1593211a4afed55b67ba07721ab5efecf44c
Aleksandr <pandom@yandex.ru>
2023-02-02 01:08:32 +0300

    001 найти точку пересеччения двух отрезков

---
+++ 001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.Clear();
+Console.WriteLine("Найти точку пересечения двух прямых заданных уравнениями");
+Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+var x = -(b1 - b2) / (k1 - k2); // приравниваем два уравнения и выносим х за равенство
+var y = k1 * x + b1;
+x = Math.Round(x, 3);//округление
+y = Math.Round(y, 3);
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
