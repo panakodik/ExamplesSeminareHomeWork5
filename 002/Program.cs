66798436bf6b82c9ec021c343d84cb782ec0aa16
Aleksandr <pandom@yandex.ru>
2023-02-02 23:54:12 +0300

    002 Фибоначчи

---
+++ 002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать числа Фибоначчи
+//F_{0}=0, F_{1}=1, F_{n}=F_{n-1}+F_{n-2},
+//n>=2
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Конечное число последовательности Фибоначчи");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("Последовательность Фибоначчи ");
+int N1 = 0;
+Console.Write(N1 + " ");
+int N2 = 1;
+Console.Write(N2 + " ");
+int sum = 0;
+for (int i = 1; number >= sum; i++)
+{
+    sum = N1 + N2;
+    Console.Write(sum + " ");
+    N1 = N2;
+    N2 = sum;
+}
