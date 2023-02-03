5312fec16a6a525b18a2219b1648e6a718c1c1bc
Aleksandr <pandom@yandex.ru>
2023-02-03 03:46:43 +0300

    004 Копия массива

---
+++ 004/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу копирования массива
+Console.Clear();
+Console.Write("- Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(""); 
+
+Console.Write("Массив:        "); 
+double[] array = new double[length];
+
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+}
+Console.WriteLine("{0}", string.Join("; ", array));
+
+Console.WriteLine(" "); 
+Console.Write("Копия массива: "); 
+double[] array1 = new double[length];
+for (int i = 0; i < length; i++)
+{
+    array1[i] = array[i];
+    Console.Write(array[i] + "; ");
+}
